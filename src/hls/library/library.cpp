3d3f1470ab7d318e80dd38a50eaf93a7eae24510
Mikhail Chupilko <chupilko@ispras.ru>
2021-10-05 17:41:24 +0300

    Just the first step toward the library.
    Signed-off-by: Mikhail Chupilko <chupilko@ispras.ru>

---
+++ src/hls/library/library.cpp
@@ -0,0 +1,12 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the Utopia EDA Project, under the Apache License v2.0
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2021 ISP RAS (http://www.ispras.ru)
+//
+//===----------------------------------------------------------------------===//
+
+#include <hls/library/library.h>
+
+namespace eda::hls::library {
+}
--- src/hls/library/library.h
+++ src/hls/library/library.h
@@ -8,5 +8,31 @@
 
 #pragma once
 
+#include <vector>
+#include <string>
+
 namespace eda::hls::library {
+
+typedef struct Port {
+  typedef enum { IN, OUT, INOUT } Direction;
+
+  Port(const std::string &name, const Direction direction, const unsigned width) :
+    name(name), direction(direction), width(width) {};
+
+  std::string name;
+  Direction direction;
+  unsigned width;
+} Port;
+
+struct ModuleInterface {
+  ModuleInterface(const std::string &name) : name(name) {};
+
+  void addPort(Port &port) {
+    interface.push_back(port);
+  };
+
+  std::string name;
+  std::vector<Port> interface;
+};
+
 } // namespace eda::hls::library
