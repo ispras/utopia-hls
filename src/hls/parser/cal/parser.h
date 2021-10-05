adf77ea13f8f3174c665fa5b4f8e49ab704c701e
Alexander Kamkin <kamkin@ispras.ru>
2021-10-05 16:20:53 +0300

    Added a folder for CAL parser.

---
+++ src/hls/parser/cal/parser.h
@@ -0,0 +1,12 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the Utopia EDA Project, under the Apache License v2.0
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2021 ISP RAS (http://www.ispras.ru)
+//
+//===----------------------------------------------------------------------===//
+
+#pragma once
+
+namespace eda::hls::parser::cal {
+} // namespace eda::hls::parser::cal
