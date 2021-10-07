ab5856f96975bb5eb4db2008be35bfb04a73a14a
Alexander Kamkin <kamkin@ispras.ru>
2021-10-07 15:37:26 +0300

    Updated hil_test.cpp.
    Signed-off-by: Alexander Kamkin <kamkin@ispras.ru>

--- test/hil_test.cpp
+++ test/hil_test.cpp
@@ -23,7 +23,7 @@ using namespace eda::hls::parser::hil;
 std::unique_ptr<Model> hil_parse(const std::string &filename) {
   if (parse(filename) == -1) {
     std::cout << "Could not parse " << filename << std::endl;
-    return NULL;
+    return nullptr;
   }
 
   return Builder::get().create();
@@ -44,29 +44,29 @@ int hil_test_graphs(const std::string &filename) {
 }
 
 int hil_test_verilogprinter(const std::string &filename) {
-  std::vector<NodeType*> nodetypes = hil_parse(filename)->nodetypes;
+  auto nodetypes = hil_parse(filename)->nodetypes;
 
   std::cout << "------ Verilog RTL-model ------" << std::endl;
-  for (const NodeType *type: nodetypes) {
-    VerilogPrinter *printer = new VerilogPrinter(*type);
+  for (const auto *nodetype: nodetypes) {
+    auto printer = std::make_unique<VerilogPrinter>(*nodetype);
     std::cout << *printer;
   }
 
   return 0;
 }
 
-TEST(HilTest, SingleTest) {
+TEST(HilTest, SimpleTest) {
   EXPECT_EQ(hil_test("test/hil/test.hil"), 0);
 }
 
-TEST(HilNodeTypesTest, SingleTest) {
+TEST(HilTest, NodeTypesTest) {
   EXPECT_EQ(hil_test_nodetypes("test/hil/test.hil"), 6);
 }
 
-TEST(HilGraphsTest, SingleTest) {
+TEST(HilTest, GraphsTest) {
   EXPECT_EQ(hil_test_graphs("test/hil/test.hil"), 1);
 }
 
-TEST(HilVerilogPrinterTest, SingleTest) {
+TEST(HilTest, VerilogPrinterTest) {
   EXPECT_EQ(hil_test_verilogprinter("test/hil/test.hil"), 0);
 }
