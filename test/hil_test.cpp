013e23dba42fe289618e7acc88003f93eb46cbf6
Alexander Kamkin <kamkin@ispras.ru>
2021-10-07 10:36:00 +0300

    Small modifications of hil_test.
    Signed-off-by: Alexander Kamkin <kamkin@ispras.ru>

--- test/hil_test.cpp
+++ test/hil_test.cpp
@@ -17,7 +17,7 @@
 using namespace eda::hls::model;
 using namespace eda::hls::parser::hil;
 
-std::unique_ptr<Model> get_model(const std::string &filename) {
+std::unique_ptr<Model> hil_parse(const std::string &filename) {
   if (parse(filename) == -1) {
     std::cout << "Could not parse " << filename << std::endl;
     return NULL;
@@ -27,19 +27,15 @@ std::unique_ptr<Model> get_model(const std::string &filename) {
 }
 
 int hil_test(const std::string &filename) {
-
-  std::cout << *get_model(filename);
-
+  std::cout << *hil_parse(filename);
   return 0;
 }
 
 int hil_test_nodetypes(const std::string &filename) {
-
   return (get_model(filename))->nodetypes.size();
 }
 
 int hil_test_graphs(const std::string &filename) {
-
   return (get_model(filename))->graphs.size();
 }
 
