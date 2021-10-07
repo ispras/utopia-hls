292e7f3ef85bc45355f5ff469714e22cc97ab488
Alexander Kamkin <kamkin@ispras.ru>
2021-10-07 20:02:06 +0300

    Replaced MLIR.cmake w/ a one from LLVM's standalone example.
    Signed-off-by: Alexander Kamkin <kamkin@ispras.ru>

--- MLIR.cmake
+++ MLIR.cmake
@@ -1,191 +1,22 @@
-# SPDX-License-Identifier: Apache-2.0
-
-if (DEFINED ENV{MLIR_DIR})
-  set(MLIR_DIR $ENV{MLIR_DIR} CACHE PATH "Path to directory containing MLIRConfig.cmake")
-elseif (NOT DEFINED MLIR_DIR)
-  message(FATAL_ERROR "MLIR_DIR is not configured but it is required. "
-          "Please set the env variable MLIR_DIR or the corresponding cmake configuration option.")
-endif()
-
 find_package(MLIR REQUIRED CONFIG)
 
 message(STATUS "Using MLIRConfig.cmake in: ${MLIR_DIR}")
 message(STATUS "Using LLVMConfig.cmake in: ${LLVM_DIR}")
 
+set(LLVM_RUNTIME_OUTPUT_INTDIR ${CMAKE_BINARY_DIR}/bin)
+set(LLVM_LIBRARY_OUTPUT_INTDIR ${CMAKE_BINARY_DIR}/lib)
+set(MLIR_BINARY_DIR ${CMAKE_BINARY_DIR})
+
 list(APPEND CMAKE_MODULE_PATH "${MLIR_CMAKE_DIR}")
 list(APPEND CMAKE_MODULE_PATH "${LLVM_CMAKE_DIR}")
-
 include(TableGen)
 include(AddLLVM)
 include(AddMLIR)
-
 include(HandleLLVMOptions)
 
 include_directories(${LLVM_INCLUDE_DIRS})
 include_directories(${MLIR_INCLUDE_DIRS})
-
+include_directories(${PROJECT_SOURCE_DIR}/include)
+include_directories(${PROJECT_BINARY_DIR}/include)
+link_directories(${LLVM_BUILD_LIBRARY_DIR})
 add_definitions(${LLVM_DEFINITIONS})
-
-set(BUILD_SHARED_LIBS ${LLVM_ENABLE_SHARED_LIBS} CACHE BOOL "" FORCE)
-message(STATUS "BUILD_SHARED_LIBS         : " ${BUILD_SHARED_LIBS})
-
-# Utopia uses exceptions, so we need to make sure that LLVM_REQUIRES_EH is set to ON, so that
-# the functions from HandleLLVMOptions and AddLLVM don't disable exceptions.
-set(LLVM_REQUIRES_EH ON)
-message(STATUS "LLVM_REQUIRES_EH          : " ${LLVM_REQUIRES_EH})
-
-# LLVM_HOST_TRIPLE is exported as part of the llvm config, so we should be able to leverage it.
-# If, for some reason, it is not set, default to an empty string which is the old default behavior of Utopia.
-set(UTOPIA_MLIR_DEFAULT_TRIPLE "${LLVM_HOST_TRIPLE}" CACHE STRING "Default triple for Utopia.")
-message(STATUS "UTOPIA_MLIR_DEFAULT_TRIPLE: " ${UTOPIA_MLIR_DEFAULT_TRIPLE})
-
-# If CMAKE_INSTALL_PREFIX was not provided explicitly and we are not using an install of
-# LLVM and a CMakeCache.txt exists,
-# force CMAKE_INSTALL_PREFIX to be the same as the LLVM build.
-if (CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT AND NOT LLVM_INSTALL_PREFIX)
-  if (EXISTS ${LLVM_BINARY_DIR}/CMakeCache.txt)
-    file(STRINGS ${LLVM_BINARY_DIR}/CMakeCache.txt prefix REGEX CMAKE_INSTALL_PREFIX)
-    string(REGEX REPLACE "CMAKE_INSTALL_PREFIX:PATH=" "" prefix ${prefix})
-    string(REGEX REPLACE "//.*" "" prefix ${prefix})
-    set(CMAKE_INSTALL_PREFIX ${prefix} CACHE PATH "" FORCE)
-  endif()
-endif()
-message(STATUS "CMAKE_INSTALL_PREFIX      : " ${CMAKE_INSTALL_PREFIX})
-
-# The tablegen functions below are modeled based on the corresponding functions
-# in mlir: https://github.com/llvm/llvm-project/blob/main/mlir/cmake/modules/AddMLIR.cmake
-function(add_utopia_mlir_dialect_doc dialect dialect_tablegen_file)
-  # Generate Dialect Documentation
-  set(LLVM_TARGET_DEFINITIONS ${dialect_tablegen_file})
-  tablegen(MLIR ${dialect}.md -gen-op-doc "-I${UTOPIA_MLIR_SRC_ROOT}")
-  set(GEN_DOC_FILE ${UTOPIA_MLIR_BIN_ROOT}/docs/Dialects/${dialect}.md)
-  add_custom_command(
-          OUTPUT ${GEN_DOC_FILE}
-          COMMAND ${CMAKE_COMMAND} -E copy
-                  ${CMAKE_CURRENT_BINARY_DIR}/${dialect}.md
-                  ${GEN_DOC_FILE}
-          DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/${dialect}.md)
-  add_custom_target(${dialect}DocGen DEPENDS ${GEN_DOC_FILE})
-  add_dependencies(utopia-mlir-doc ${dialect}DocGen)
-endfunction()
-add_custom_target(utopia-mlir-doc)
-
-function(add_utopia_mlir_dialect dialect)
-  set(LLVM_TARGET_DEFINITIONS ${dialect}.td)
-  mlir_tablegen(${dialect}.hpp.inc -gen-op-decls "-I${UTOPIA_MLIR_SRC_ROOT}")
-  mlir_tablegen(${dialect}.cpp.inc -gen-op-defs "-I${UTOPIA_MLIR_SRC_ROOT}")
-  add_public_tablegen_target(OM${dialect}IncGen)
-endfunction()
-
-function(add_utopia_mlir_rewriter rewriter)
-  set(LLVM_TARGET_DEFINITIONS ${rewriter}.td)
-  mlir_tablegen(UTOPIA${rewriter}.inc -gen-rewriters "-I${UTOPIA_MLIR_SRC_ROOT}")
-  add_public_tablegen_target(OMUTOPIA${rewriter}IncGen)
-endfunction()
-
-function(add_utopia_mlir_interface interface)
-  set(LLVM_TARGET_DEFINITIONS ${interface}.td)
-  mlir_tablegen(${interface}.hpp.inc -gen-op-interface-decls)
-  mlir_tablegen(${interface}.cpp.inc -gen-op-interface-defs)
-  add_public_tablegen_target(OM${interface}IncGen)
-endfunction()
-
-# add_utopia_mlir_library(name sources...
-#   This function (generally) has the same semantic as add_library. In
-#   addition is supports the arguments below and it does the following
-#   by default (unless an argument overrides this):
-#   1. Add the library
-#   2. Add the default target_include_directories
-#   3. Add the library to a global property UTOPIA_MLIR_LIBS
-#   4. Add an install target for the library
-#   EXCLUDE_FROM_OM_LIBS
-#     Do not add the library to the UTOPIA_MLIR_LIBS property.
-#   NO_INSTALL
-#     Do not add an install target for the library.
-#   DEPENDS targets...
-#     Same semantics as add_dependencies().
-#   INCLUDE_DIRS include_dirs...
-#     Same semantics as target_include_directories().
-#   LINK_LIBS lib_targets...
-#     Same semantics as target_link_libraries().
-#   )
-function(add_utopia_mlir_library name)
-  cmake_parse_arguments(ARG
-    "EXCLUDE_FROM_OM_LIBS;NO_INSTALL"
-    ""
-    "DEPENDS;INCLUDE_DIRS;LINK_LIBS"
-    ${ARGN}
-    )
-
-  if (NOT ARG_EXCLUDE_FROM_OM_LIBS)
-    set_property(GLOBAL APPEND PROPERTY UTOPIA_MLIR_LIBS ${name})
-  endif()
-
-  add_library(${name} ${ARG_UNPARSED_ARGUMENTS})
-  llvm_update_compile_flags(${name})
-
-  if (ARG_DEPENDS)
-    add_dependencies(${name} ${ARG_DEPENDS})
-  endif()
-
-  if (ARG_INCLUDE_DIRS)
-    target_include_directories(${name} ${ARG_INCLUDE_DIRS})
-  endif()
-
-  target_include_directories(${name}
-    PUBLIC
-    ${UTOPIA_MLIR_SRC_ROOT}
-    ${UTOPIA_MLIR_BIN_ROOT}
-    )
-
-  if (ARG_LINK_LIBS)
-    target_link_libraries(${name} ${ARG_LINK_LIBS})
-  endif()
-
-  if (NOT ARG_NO_INSTALL)
-    install(TARGETS ${name} DESTINATION lib)
-  endif()
-endfunction(add_utopia_mlir_library)
-
-# add_utopia_mlir_executable(name sources...
-#   This function (generally) has the same semantic as add_executable.
-#   In addition is supports the arguments below and it does the following
-#   by default (unless an argument overrides this):
-#   1. Add the executable
-#   2. Add an install target for the executable
-#   NO_INSTALL
-#     Do not add an install target for the executable.
-#   DEPENDS targets...
-#     Same semantics as add_dependencies().
-#   INCLUDE_DIRS include_dirs...
-#     Same semantics as target_include_directories().
-#   LINK_LIBS lib_targets...
-#     Same semantics as target_link_libraries().
-#   )
-function(add_utopia_mlir_executable name)
-  cmake_parse_arguments(ARG
-    "NO_INSTALL"
-    ""
-    "DEPENDS;INCLUDE_DIRS;LINK_LIBS"
-    ${ARGN}
-    )
-
-  add_executable(${name} ${ARG_UNPARSED_ARGUMENTS})
-  llvm_update_compile_flags(${name})
-
-  if (ARG_DEPENDS)
-    add_dependencies(${name} ${ARG_DEPENDS})
-  endif()
-
-  if (ARG_INCLUDE_DIRS)
-    target_include_directories(${name} ${ARG_INCLUDE_DIRS})
-  endif()
-
-  if (ARG_LINK_LIBS)
-    target_link_libraries(${name} ${ARG_LINK_LIBS})
-  endif()
-
-  if (NOT ARG_NO_INSTALL)
-    install(TARGETS ${name} DESTINATION bin)
-  endif()
-endfunction(add_utopia_mlir_executable)
