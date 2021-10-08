96d70f68098d4c397a3d18eb6631c3e3ab14634d
Sergey Smolov <smolov@ispras.ru>
2021-10-08 11:47:37 +0300

    debugger: initial version that does nothing (#10966)
    libz3-dev is added to project dependencies
    
    Signed-off-by: Sergey Smolov <smolov@ispras.ru>

---
+++ src/hls/debugger/debugger.cpp
@@ -0,0 +1,51 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the Utopia EDA Project, under the Apache License v2.0
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2021 ISP RAS (http://www.ispras.ru)
+//
+//===----------------------------------------------------------------------===//
+
+#include <hls/debugger/debugger.h>
+
+#include "z3++.h"
+
+using namespace eda::hls::model;
+using namespace z3;
+
+namespace eda::hls::debugger {
+
+  std::unique_ptr<Verifier> Verifier::instance = nullptr;
+
+  bool Verifier::equivalent(Model &left, Model &right) const {
+
+    std::vector<Graph *> leftGraphs = left.graphs;
+    std::vector<Graph *> rightGraphs = right.graphs;
+
+    if (leftGraphs.size() != rightGraphs.size())
+      return false;
+    // TODO: check graph vectors here
+
+
+    context ctx;
+
+    // TODO: add function declarations here
+
+    solver s(ctx);
+
+    // TODO: add formulae here
+
+    switch (s.check()) {
+      case sat:
+        std::cout << "Models are equivalent" << std::endl;
+        return true;
+      case unsat:
+        std::cout << "Models are not equivalent" << std::endl;
+        return false;
+      case unknown:
+      default:
+        std::cout << "Z3 solver says \"unknown\"" << std::endl;
+        break;
+    }
+  }
+} // namespace eda::hls::debugger
--- src/hls/debugger/debugger.h
+++ src/hls/debugger/debugger.h
@@ -8,5 +8,31 @@
 
 #pragma once
 
+#include "hls/model/model.h"
+
+#include <memory>
+
+using namespace eda::hls::model;
+
 namespace eda::hls::debugger {
+
+class Verifier final {
+
+public:
+  static Verifier& get() {
+    if (instance == nullptr) {
+      instance = std::unique_ptr<Verifier>(new Verifier());
+    }
+    return *instance;
+  }
+
+  bool equivalent(Model &left, Model &right) const;
+
+private:
+  Verifier() {}
+
+  static std::unique_ptr<Verifier> instance;
+
+  expr to_expr(Model &model) const;
+};
 } // namespace eda::hls::debugger
