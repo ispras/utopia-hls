6b13b9784eabc75de43bc6c1facf2faa89b54776
helena2505 <elenakarelina833@gmail.com>
2021-11-06 14:26:42 +0300

    launch scheduler

--- src/hls/scheduler/param_optimizer.cpp
+++ src/hls/scheduler/param_optimizer.cpp
@@ -11,6 +11,9 @@
 #include "hls/scheduler/param_optimizer.h"
 
 #include <cassert>
+#include <iostream>
+#include <fstream>
+#include <string>
 
 namespace eda::hls::scheduler {
 
@@ -31,20 +34,24 @@ std::map<std::string, Parameters> ParametersOptimizer::optimize(
     params.insert({ node->name, nodeParams });
   }
 
+  int iter_counter = 0;
+  std::ofstream ostrm("debug_file.txt", std::ios::out);
+  ostrm << "Running optimization loop" << std::endl;
   // Optimization loop.
   while (true) {
+    iter_counter++;
     // Update the values of the parameters.
     for (const auto *node : graph->nodes) {
       auto i = params.find(node->name);
       for (auto param : i->second.params) {
-        param.second.value++; // FIXME
+        param.second.value += 100; // FIXME
       }
     }
 
     // Balance flows and align times.
     // TODO:
     indicators.latency = 100; // FIXME
-    indicators.frequency = 100000; // FIXME
+    indicators.frequency = 100000 * iter_counter; // FIXME
 
     // Estimate the integral indicators.
     indicators.throughput = indicators.frequency;
@@ -56,20 +63,35 @@ std::map<std::string, Parameters> ParametersOptimizer::optimize(
       Indicators nodeIndicators;
       Library::get().estimate(i->second, nodeIndicators);
 
+      //ostrm << i->second.value("f") << std::endl;
+      ostrm << nodeIndicators.frequency << std::endl;
+
       indicators.power += nodeIndicators.power;
       indicators.area += nodeIndicators.area;
     }
-
+    ostrm << std::endl << "Frequency: " << indicators.frequency << std::endl;
+    ostrm << "Latency: " << indicators.latency << std::endl;
+    ostrm << "Throughput: " << indicators.throughput << std::endl;
+    ostrm << "Power: " << indicators.power << std::endl;
+    ostrm << "Area: " << indicators.area << std::endl << std::endl;
+    
     // Check the constraints.
     if (criteria.check(indicators)) {
       // Acceptable.
-      break; // FIXME
+      ostrm << "Solution found" << std::endl;
+      break;
     } else {
       // Unacceptable.
-      break; // FIXME
+      if(iter_counter > 10) {
+        ostrm << "Solution has not been found" << std::endl;
+        break;
+      }
     }
+    //break;
   }
 
+  ostrm << "Number of iterations: " << iter_counter << std::endl;
+  ostrm.close();
   return params;
 }
 
