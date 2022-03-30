d302320ea802f967749424e0570dfbd14d58fa34
Sergey Smolov <smolov@ispras.ru>
2022-03-30 16:34:17 +0300

    mlir: test_transforms.cpp -> transform_test.cpp
    Signed-off-by: Sergey Smolov <smolov@ispras.ru>

--- test/hls/model/mlir/transform_test.cpp
+++ test/hls/model/mlir/transform_test.cpp
@@ -1,3 +1,11 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the Utopia EDA Project, under the Apache License v2.0
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2021-2022 ISP RAS (http://www.ispras.ru)
+//
+//===----------------------------------------------------------------------===//
+
 #include "HIL/API.h"
 #include "HIL/Dumper.h"
 #include "hls/model/model.h"
