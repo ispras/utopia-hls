dc3975902afe3364ade14e5265c57b80d77a588d
Alexander Kamkin <kamkin@ispras.ru>
2022-06-20 20:58:08 +0300

    Slightly changed tool-run.sh.
    Signed-off-by: Alexander Kamkin <kamkin@ispras.ru>

--- tool-run.sh
+++ tool-run.sh
@@ -4,13 +4,13 @@
 
 ./build/src/umain rtl test/data/ril/test.ril
 
-./build/src/umain hls --output-dir ./output \
+./build/src/umain hls --output-dir ./output/ \
                       --output-mlir test.mlir \
                       --output-vlog test.v \
                       --output-test test_tb.v \
                       test/data/hil/test.hil
 
-./build/src/umain hls --output-dir ./output \
+./build/src/umain hls --output-dir ./output/ \
                       --output-mlir idct.mlir \
                       --output-vlog idct.v \
                       --output-test idct_tb.v \
