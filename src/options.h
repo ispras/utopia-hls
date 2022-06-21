632499e4c1424b11c7e263125ea72b4c02f2a98f
Alexander Kamkin <kamkin@ispras.ru>
2022-06-21 19:43:44 +0300

    Implemented support of JSON config (incomplete).
    Signed-off-by: Alexander Kamkin <kamkin@ispras.ru>

--- src/options.h
+++ src/options.h
@@ -11,8 +11,12 @@
 #include "CLI/CLI.hpp"
 #include "nlohmann/json.hpp"
 
+#include <fstream>
+#include <iostream>
 #include <string>
 #include <vector>
+
+using Json = nlohmann::json;
 
 class AppOptions {
 public:
@@ -30,17 +34,55 @@ public:
     if (isRoot) { delete options; }
   }
 
-  std::string toConfig() const {
-    return toConfig(options);
+  void parse(int argc, char **argv) {
+    options->parse(argc, argv);
+  }
+
+  virtual void fromJson(Json json) {
+    // TODO: Default implementation.
+  }
+
+  virtual Json toJson() const {
+    return toJson(options);
+  }
+
+  virtual void read(std::istream &in) {
+    auto json = Json::parse(in);
+    fromJson(json);
+  }
+
+  virtual void save(std::ostream &out) const {
+    auto json = toJson();
+    out << json;
+  }
+
+  void read(const std::string &config) {
+    std::ifstream in(config);
+    if (in.good()) {
+      read(in);
+    }
+  }
+
+  void save(const std::string &config) const {
+    std::ofstream out(config);
+    if (out.good()) {
+      save(out);
+    }
   }
 
 protected:
-  const bool isRoot;
-  CLI::App *options;
+  static std::string cli(const std::string &option) {
+    return "--" + option;
+  }
 
-private:
-  std::string toConfig(const CLI::App *app) const {
-    nlohmann::json json;
+  static void get(Json json, const std::string &key, std::string &value) {
+    if (json.contains(key)) {
+      value = json[key].get<std::string>();
+    }
+  }
+
+  Json toJson(const CLI::App *app) const {
+    Json json;
 
     for (const auto *opt : app->get_options({})) {
       if (!opt->get_lnames().empty() && opt->get_configurable()) {
@@ -65,16 +107,21 @@ private:
     }
 
     for(const auto *cmd : app->get_subcommands({})) {
-      json[cmd->get_name()] = nlohmann::json(toConfig(cmd));
+      json[cmd->get_name()] = Json(toJson(cmd));
     }
 
-    return json.dump(2);
+    return json;
   }
+
+  const bool isRoot;
+  CLI::App *options;
 };
 
 struct RtlOptions final : public AppOptions {
+  static constexpr const char *ID = "rtl";
+
   RtlOptions(AppOptions &parent):
-      AppOptions(parent, "rtl", "Logical synthesis") {
+      AppOptions(parent, ID, "Logical synthesis") {
 
     // Input file(s).
     options->allow_extras();
@@ -86,19 +133,27 @@ struct RtlOptions final : public AppOptions {
 };
 
 struct HlsOptions final : public AppOptions {
+  static constexpr const char *ID = "hls";
+
+  static constexpr const char *OUTPUT_DIR  = "output-dir";
+  static constexpr const char *OUTPUT_DOT  = "output-dot";
+  static constexpr const char *OUTPUT_MLIR = "output-mlir";
+  static constexpr const char *OUTPUT_VLOG = "output-vlog";
+  static constexpr const char *OUTPUT_TEST = "output-test";
+
   HlsOptions(AppOptions &parent):
-      AppOptions(parent, "hls", "High-level synthesis") {
+      AppOptions(parent, ID, "High-level synthesis") {
 
     // Named options.
-    options->add_option("--output-dir",  outDir,  "Output directory")
+    options->add_option(cli(OUTPUT_DIR),  outDir,  "Output directory")
            ->expected(1);
-    options->add_option("--output-dot",  outDot,  "Output DOT file")
+    options->add_option(cli(OUTPUT_DOT),  outDot,  "Output DOT file")
            ->expected(0, 1);
-    options->add_option("--output-mlir", outMlir, "Output MLIR file")
+    options->add_option(cli(OUTPUT_MLIR), outMlir, "Output MLIR file")
            ->expected(1);
-    options->add_option("--output-vlog", outVlog, "Output Verilog file")
+    options->add_option(cli(OUTPUT_VLOG), outVlog, "Output Verilog file")
            ->expected(1);
-    options->add_option("--output-test", outTest, "Output test file")
+    options->add_option(cli(OUTPUT_TEST), outTest, "Output test file")
            ->expected(0, 1);
 
     // Input file(s).
@@ -107,6 +162,14 @@ struct HlsOptions final : public AppOptions {
 
   std::vector<std::string> files() const {
     return options->remaining();
+  }
+
+  void fromJson(Json json) override {
+    get(json, OUTPUT_DIR,  outDir);
+    get(json, OUTPUT_DOT,  outDot);
+    get(json, OUTPUT_MLIR, outMlir);
+    get(json, OUTPUT_VLOG, outVlog);
+    get(json, OUTPUT_TEST, outTest);
   }
 
   std::string outDir;
@@ -119,6 +182,7 @@ struct HlsOptions final : public AppOptions {
 struct Options final : public AppOptions {
   Options(const std::string &title,
           const std::string &version,
+          const std::string &config,
           int argc, char **argv):
       AppOptions(title), rtl(*this), hls(*this) {
 
@@ -126,7 +190,16 @@ struct Options final : public AppOptions {
     options->set_help_all_flag("--help-all", "Print help");
     options->set_version_flag("--version", version, "Print version");
 
-    options->parse(argc, argv);
+    // Read the configuration file.
+    read(config);
+
+    // Parse the command line.
+    parse(argc, argv);
+  }
+
+  void fromJson(Json json) override {
+    rtl.fromJson(json[RtlOptions::ID]);
+    hls.fromJson(json[HlsOptions::ID]);
   }
 
   RtlOptions rtl;
