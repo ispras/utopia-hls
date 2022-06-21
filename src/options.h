769d4f06d1e8bdb8688d61efb1854d2702513457
Alexander Kamkin <kamkin@ispras.ru>
2022-06-21 16:36:34 +0300

    Added N.Lohmann's JSON parser.
    Signed-off-by: Alexander Kamkin <kamkin@ispras.ru>

--- src/options.h
+++ src/options.h
@@ -9,15 +9,72 @@
 #pragma once
 
 #include "CLI/CLI.hpp"
+#include "nlohmann/json.hpp"
 
 #include <string>
 #include <vector>
 
-struct RtlOptions final {
-  RtlOptions() = delete;
+class AppOptions {
+public:
+  AppOptions() = delete;
 
-  RtlOptions(CLI::App &app) {
-    options = app.add_subcommand("rtl", "Logical synthesis");
+  AppOptions(const std::string &title):
+      isRoot(true), options(new CLI::App(title)) {}
+
+  AppOptions(AppOptions &parent,
+             const std::string &cmd,
+             const std::string &desc):
+      isRoot(false), options(parent.options->add_subcommand(cmd, desc)) {}
+
+  virtual ~AppOptions() {
+    if (isRoot) { delete options; }
+  }
+
+  std::string toConfig() const {
+    return toConfig(options);
+  }
+
+protected:
+  const bool isRoot;
+  CLI::App *options;
+
+private:
+  std::string toConfig(const CLI::App *app) const {
+    nlohmann::json json;
+
+    for (const auto *opt : app->get_options({})) {
+      if (!opt->get_lnames().empty() && opt->get_configurable()) {
+        const auto name = opt->get_lnames()[0];
+
+        if (opt->get_type_size() != 0) {
+          if (opt->count() == 1) {
+            json[name] = opt->results().at(0);
+          } else if (opt->count() > 1) {
+            json[name] = opt->results();
+          } else if (!opt->get_default_str().empty()) {
+            json[name] = opt->get_default_str();
+          }
+        } else if (opt->count() == 1) {
+          json[name] = true;
+        } else if (opt->count() > 1) {
+          json[name] = opt->count();
+        } else if (opt->count() == 0) {
+          json[name] = false;
+        }
+      }
+    }
+
+    for(const auto *cmd : app->get_subcommands({})) {
+      json[cmd->get_name()] = nlohmann::json(toConfig(cmd));
+    }
+
+    return json.dump(2);
+  }
+};
+
+struct RtlOptions final : public AppOptions {
+  RtlOptions(AppOptions &parent):
+      AppOptions(parent, "rtl", "Logical synthesis") {
 
     // Input file(s).
     options->allow_extras();
@@ -26,17 +83,13 @@ struct RtlOptions final {
   std::vector<std::string> files() const {
     return options->remaining();
   }
-
-private:
-  CLI::App *options;
 };
 
-struct HlsOptions final {
-  HlsOptions() = delete;
+struct HlsOptions final : public AppOptions {
+  HlsOptions(AppOptions &parent):
+      AppOptions(parent, "hls", "High-level synthesis") {
 
-  HlsOptions(CLI::App &app) {
-    options = app.add_subcommand("hls", "High-level synthesis");
-
+    // Named options.
     options->add_option("--output-dir",  outDir,  "Output directory")
            ->expected(1);
     options->add_option("--output-dot",  outDot,  "Output DOT file")
@@ -61,27 +114,21 @@ struct HlsOptions final {
   std::string outMlir;
   std::string outVlog;
   std::string outTest;
-
-private:
-  CLI::App *options;
 };
 
-struct Options final {
-  Options() = delete;
-
+struct Options final : public AppOptions {
   Options(const std::string &title,
           const std::string &version,
-          int argc, char **argv): app(title), rtl(app), hls(app) {
-    app.set_help_all_flag("--help-all", "Print help");
-    app.set_version_flag("--version", version, "Print version");
+          int argc, char **argv):
+      AppOptions(title), rtl(*this), hls(*this) {
 
-    app.parse(argc, argv);
+    // Top-level options.
+    options->set_help_all_flag("--help-all", "Print help");
+    options->set_version_flag("--version", version, "Print version");
+
+    options->parse(argc, argv);
   }
 
-private:
-  CLI::App app;
-
-public:
   RtlOptions rtl;
   HlsOptions hls;
 };
