339fe9e0115d33b5c71fe8bab2be5009f125a7ed
Ivan Grigorov <grigorovia@ispras.ru>
2022-08-01 17:02:24 +0300

    fix run-tests.sh.
    Signed-off-by: Ivan Grigorov <grigorovia@ispras.ru>

--- run-tests.sh
+++ run-tests.sh
@@ -2,9 +2,4 @@
 
 # SPDX-License-Identifier: Apache-2.0
 
-<<<<<<< Updated upstream
 ./build/test/utest --gtest_filter=-*BigPartitionTest*
-=======
-rm -rf ./output
-./build/test/utest #--gtest_filter=-CheckNetlistTest.*
->>>>>>> Stashed changes
