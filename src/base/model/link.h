4171153011bd8ed4070414fa2c4d5134674e96ba
Alexander Kamkin <kamkin@ispras.ru>
2022-10-11 18:45:29 +0300

    Started refactoring RTL and gate-level nets and separating common classes.
    Signed-off-by: Alexander Kamkin <kamkin@ispras.ru>

---
+++ src/base/model/link.h
@@ -0,0 +1,63 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the Utopia EDA Project, under the Apache License v2.0
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2021 ISP RAS (http://www.ispras.ru)
+//
+//===----------------------------------------------------------------------===//
+
+#pragma once
+
+#include <algorithm>
+#include <cstddef>
+
+namespace eda::base::model {
+
+/// Represents a connection between two nodes.
+template<typename N>
+struct Link final {
+  // General link.
+  Link(N source, N target, std::size_t input):
+    source(source), target(target), input(input) {}
+
+  // Self-link (a port).
+  explicit Link(N node): Link(node, node, 0) {}
+
+  bool isPort() const {
+    return source == target;
+  }
+
+  bool operator ==(const Link<N> &rhs) const {
+    return source == rhs.source && target == rhs.target && input == rhs.input;
+  }
+
+  /// Source node.
+  N source;
+  /// Target node.
+  N target;
+  /// Target input.
+  std::size_t input;
+};
+
+} // namespace eda::base::model
+
+//===----------------------------------------------------------------------===//
+// Hash
+//===----------------------------------------------------------------------===//
+
+namespace std {
+
+/// Hash for the Link class.
+template <typename N>
+struct hash<eda::base::model::Link<N>> {
+  std::size_t operator()(const eda::base::model::Link<N> &link) const {
+    std::size_t h = std::hash<N>()(link.source);
+    h *= 37;
+    h += std::hash<N>()(link.target);
+    h *= 37;
+    h += link.input;
+    return h;
+  }
+};
+
+} // namespace std
