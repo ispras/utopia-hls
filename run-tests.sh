0474d089c870d1ffcb7f816393035f80a677fefd
Sergey Smolov <smolov@ispras.ru>
2024-08-15 14:29:12 +0300

    run-tests.sh: build project with tests
    Signed-off-by: Sergey Smolov <smolov@ispras.ru>

--- run-tests.sh
+++ run-tests.sh
@@ -2,4 +2,16 @@
 
 # SPDX-License-Identifier: Apache-2.0
 
+# Build project with tests
+
+rm -rf build
+
+cmake -S . -B build -G Ninja \
+      -DCMAKE_PREFIX_PATH="/home/ssedai/sources/circt.git/build;/home/ssedai/sources/circt.git/llvm/build" \
+      -DSRC_FILES="~/projects/utopia-hls/examples/polynomial2/polynomial2.cpp" \
+      -DBUILD_TESTS=ON #-DCMAKE_CXX_COMPILER=clang++
+
+cmake --build build
+
+# Run tests
 build/test/utest
