d988c850588dad25c4dce36b7a0675c39b22a018
Litvinov Mikhail <litvinov@ispras.ru>
2025-02-10 00:33:23 +0300

    MagmaDecoder: implementation was added and a GOST testbench was added.

---
+++ examples/magma/magma_decoder.cpp
@@ -0,0 +1,16 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the Utopia HLS Project, under the Apache License v2.0
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2025 ISP RAS (http://www.ispras.ru)
+//
+//===----------------------------------------------------------------------===//
+
+#include "magma_decoder.h"
+
+#include <memory>
+
+std::unique_ptr<dfcxx::Kernel> start() {
+  MagmaDecoder *kernel = new MagmaDecoder();
+  return std::unique_ptr<dfcxx::Kernel>(kernel);
+}
