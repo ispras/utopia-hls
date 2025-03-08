f3f0c08073b745be2ed1decbd5f38e5bf54f204d
Litvinov Mikhail <litvinov@ispras.ru>
2025-03-08 20:13:01 +0300

    Global license headers fix.

--- src/model/dfcxx/include/dfcxx/control.h
+++ src/model/dfcxx/include/dfcxx/control.h
@@ -2,7 +2,7 @@
 //
 // Part of the Utopia HLS Project, under the Apache License v2.0
 // SPDX-License-Identifier: Apache-2.0
-// Copyright 2021-2024 ISP RAS (http://www.ispras.ru)
+// Copyright 2024 ISP RAS (http://www.ispras.ru)
 //
 //===----------------------------------------------------------------------===//
 
