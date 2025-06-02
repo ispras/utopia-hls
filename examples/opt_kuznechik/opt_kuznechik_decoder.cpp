364003a885526da23729c10409f4c3d1ad5e7d84
Muxianesty <71048120+Muxianesty@users.noreply.github.com>
2025-06-02 18:11:18 +0300

    Examples: Optimized "Kuznechik" (#75)
    * CI: Added optimized "Kuznechik" implementation tests to scheduling workflow.
    
    * Tests: added optimized "Kuznechik" algorithm implementation.
    * Tests, CI: Kuznechik{Encoder,Decoder}Opt -> OptKuznechik{Encoder,Decoder}.
    
    * examples/opt_kuznechik/opt_kuznechik_decoder.cpp: fixed a typo in #include.
    
    ---------
    
    Co-authored-by: Muxianesty <litvinov@ispras.ru>

---
+++ examples/opt_kuznechik/opt_kuznechik_decoder.cpp
@@ -0,0 +1,16 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the Utopia HLS Project, under the Apache License v2.0
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2025 ISP RAS (http://www.ispras.ru)
+//
+//===----------------------------------------------------------------------===//
+
+#include "opt_kuznechik_decoder.h"
+
+#include <memory>
+
+std::unique_ptr<dfcxx::Kernel> start() {
+  OptKuznechikDecoder *kernel = new OptKuznechikDecoder();
+  return std::unique_ptr<dfcxx::Kernel>(kernel);
+}
